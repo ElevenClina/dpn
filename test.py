88a9f21ecee8c8c0c13f1bb14c7a44b6ced10331
easy <2511384642@qq.com>
2023-05-15 10:06:44 +0800

    模型测试

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
+import torch
 from model.net import DQN
 from tankbattle.env.engine import TankBattle
 from tankbattle.env.utils import Utils
 
-max_iterator = 100
+
+device = "cpu"
+max_iterator = 1
+epsilon = 0.7647789637410702
+data_save_path = "output/test/"
+param_file = "param/predicted_402_0.7647789637410702_50.pth"
 
 
-def test(game, net, init_state, epsilon, device):
+def test(game, net, init_state, epsilon, device, iteration=0):
     total_reward = 0.0
     state = init_state
+    index = 0
+
     while True:
         game.render()
         action = net.act(state, epsilon, device)
@@ -15,12 +23,17 @@ def test(game, net, init_state, epsilon, device):
         next_state = Utils.resize_image(next_state)
         reward = reward[0]
 
+        """ 保存交互数据，根据需求自行定义"""
+        # runtime_data = ("state", action, reward, "next_state", done)
+        # Utils.save_data(runtime_data, f"{data_save_path}test_data_{iteration}_{index}.txt")
+
         total_reward += reward
 
         if done:
             print(total_reward)
             break
         state = next_state
+        index += 1
 
 
 if __name__ == "__main__":
@@ -31,7 +44,7 @@ if __name__ == "__main__":
         init_state = game.reset()
         state = Utils.resize_image(init_state)
         origin_net = DQN(input_shape=state.shape, num_actions=game.get_num_of_actions())
-        net = Utils.load_model(origin_net, "output/predicted_402_0.7647789637410702_50.pth")
-        test(game, net, state, 0.7647789637410702, "cpu")
+        net = Utils.load_model(origin_net, param_file)
+        test(game, net, state, epsilon, device, iteration=i)
 
 
